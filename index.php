60c73b6cbf1621b6430fe1431fa1560dc376c632
anyakul <anyakulikova111@gmail.com>
2019-04-23 13:39:29 +0300

    Добавлена функция форматирования цены

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 $is_auth = rand(0, 1);
-
-$user_name = ''; // укажите здесь ваше имя
+$user_name = "Аня Куликова"; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -26,42 +25,124 @@ $user_name = ''; // укажите здесь ваше имя
         </form>
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
-        <nav class="user-menu">
-
+        <nav class="user-menu">                         
+		                      
+		                                                     
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+		
+		<!-- данные пользователя-->
+		
+		<?php if ($is_auth==1): ?>  
+		  <div class="user-menu__logged">
+		     <p><?= $user_name ?></p>
+		     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+		     <a class="user-menu__logout" href="#">Выход</a>
+		   </div> 
+        <?php elseif ($is_auth==0) : ?>		    
+			<ul class="user-menu__list">
+		      <li class="user-menu__item">
+		        <a href="#">Регистрация</a>
+		      </li>
+		      <li class="user-menu__item">
+		         <a href="#">Вход</a>
+		      </li>
+			</ul> 
+		<?php endif; ?>                                          
+		
         </nav>
     </div>
 </header>
 
 <main class="container">
+
+ <!-- Создаем  массивы   -->
+ 
+    <?php 
+	$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"]; 
+		  
+ 		$goods = [ 
+		   [
+		       'name' => '2014 Rossignol District Snowboard',
+		       'category' => $categories[0],
+		       'price' => 10999,
+		       'url' => 'img/lot-1.jpg'
+		    ],
+ 		    [
+		       'name' => 'DC Ply Mens 2016/2017 Snowboard',
+		       'category' => $categories[0],
+		       'price' => 159999,
+		       'url' => 'img/lot-2.jpg'
+		    ],
+		    [
+		       'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+		       'category' => $categories[1],
+		       'price' => 8000,
+		       'url' => 'img/lot-3.jpg'
+		    ],
+		    [
+		       'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+		       'category' => $categories[2],
+		       'price' => 10999,
+		       'url' => 'img/lot-4.jpg'
+		    ],
+		    [
+		       'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+		       'category' => $categories[3],
+		       'price' => 7500,
+		       'url' => 'img/lot-5.jpg'
+		    ],
+		    [
+		       'name' => 'Маска Oakley Canopy',
+		       'category' => $categories[5],
+		       'price' => 400,
+		       'url' => 'img/lot-6.jpg'
+		    ] 
+		];
+  		
+    ?>
+	<!-- Добавляем функцию форматирования цены-->
+	
+    <?php function format_price($var) {
+              $var = ceil($var);
+              $str = number_format($var, 0, ' ', ' '); 
+		      return $str;
+	       }
+	?>
     <section class="promo">
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
-            </li>
+		
+            <!--заполняем этот список из массива категорий-->
+			
+			<?php	foreach ($categories as $key => $val): ?>
+               <li class="promo__item promo__item--boards">
+                  <a class="promo__link" href="pages/all-lots.html"><?=$val?></a>
+               </li>
+			 <?php endforeach; ?>
+			 
         </ul>
     </section>
     <section class="lots">
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
-        <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+		<ul class="lots__list">	
+        
+            <!--заполняем этот список из массива с товарами-->	
+			
+			<?php	foreach ($goods as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$val['url']?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$val['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_price($val['price'] ) ." ₽"?>  </span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -69,6 +150,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+			<?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -77,10 +159,15 @@ $user_name = ''; // укажите здесь ваше имя
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
+		
+            <!--заполняем этот список из массива категорий-->
+			
+		<?php	foreach ($categories as $key => $val): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$val?></a>
             </li>
+		<?php endforeach; ?>
+		      
         </ul>
     </nav>
     <div class="main-footer__bottom container">
