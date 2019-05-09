651452a03476c605a467404230dd2061aae07516
anyakul <anyakulikova111@gmail.com>
2019-05-09 16:30:24 +0300

    Регистрация на сайте

--- sign-up.php
+++ sign-up.php
@@ -42,14 +42,14 @@
 				}
 				if ($key == "email" and !filter_var($value, FILTER_VALIDATE_EMAIL)) {
 					$errors[$key] = 'Email должен быть корректным';
-					if (isset($users['email'])) {
+					if (isset($user['email'])) {
 						$errors[$key] = 'Данный email уже использовался. попробуйте войти в аккаунт';
 					}
 				}
                 // Если ошибок нет, то сохранить данные формы в таблице пользователей
 			    if (count($errors) == 0) {
 					foreach ($required as $key) {
-						$lot[$key] = addslashes($lot[$key]);
+						$user[$key] = addslashes($user[$key]);
 					}
 					$users['password'] = password_hash('secret-password', PASSWORD_DEFAULT);
 					$sql = "INSERT INTO users (date_registration, email, password, name, contacts) 
